3eec46418082d50fb9b3cb57dc94b7ad268fc8b2
juulman <manpreet@juul.com>
2018-10-21 14:30:14 -0700

    Add Credit card model and association
    We want a model to persist the credit card info. We do not
    want to fetch the attributes from braintree everytime.
    Hence we will cache/persist them in db.

---
+++ db/migrate/20181015212440_add_payment_details_to_source.rb
@@ -0,0 +1,13 @@
+class AddBraintreePaymentMethodToSource < SolidusSupport::Migration[4.2]
+  def up
+    change_table :solidus_paypal_braintree_sources do |t|
+      t.references :payment_details, polymorphic: true
+    end
+  end
+
+  def down
+    change_table :solidus_paypal_braintree_sources do |t|
+      t.remove_references :payment_details, polymorphic: true
+    end
+  end
+end
